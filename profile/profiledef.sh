d2c7fdd4b87a7fabc7f4705755b7309441b1a43c
Aabish Malik <aabishmalik3337@gmail.com>
2024-01-09 22:24:20 +0530

    remove syslinux, pure uefi boot

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -8,13 +8,15 @@ iso_application="GoonArch Live/Rescue CD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+bootmodes=('uefi-x64.grub.esp'
+           'uefi-x64.grub.eltorito'
+	   'uefi-x64.systemd-boot.esp'
+	   'uefi-x64.systemd-boot.eltorito')
+
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_type="ext4+squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-b' '1M' '-Xbcj' 'x86')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
@@ -24,17 +26,7 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/home/goonarch/.local/bin/wallpaper"]="0:0:755"
-  ["/home/goonarch/.local/bin/colorscript"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/clamshell.sh"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/import-gsettings"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/lock.sh"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/powermenu.sh"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/screenshot.sh"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/sleep.sh"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/start-swww.sh"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/start-waybar.sh"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/toggle-mode.sh"]="0:0:755"
-  ["/home/goonarch/.config/hypr/scripts/xdg-nuke.sh"]="0:0:755"
+  ["/home/goonarch/.local/bin/"]="0:0:755"
+  ["/home/goonarch/.config/hypr/scripts/"]="0:0:755"
 
 )
